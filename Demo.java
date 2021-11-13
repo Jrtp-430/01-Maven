c20a360e5232115c1f611f6d0f51ef9d3db64bad
Jrtp-430 <ramaraogartam430@gmail.com>
2021-11-13 11:57:45 -0700

    mi() method added

--- Demo.java
+++ Demo.java
@@ -1,5 +1,9 @@
 public class Demo{
 	public static void main(String[]args){
 		System.out.println("hi");
-{
+{        
+         public void m1(){
+                System.out.println("hi");
+                    
+}
 }
